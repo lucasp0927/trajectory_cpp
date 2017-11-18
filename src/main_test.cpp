31b65a146b54d6700e042467431ccdc13783f597
Lucas Peng <lucasp0927@gmail.com>
2017-11-17 16:38:22 -0800

    make main_test compileable.

--- src/main_test.cpp
+++ src/main_test.cpp
@@ -3,19 +3,22 @@
 #include <gtest/gtest.h>
 #include "ConfigParser.h"
 #include "Fields.h"
+#include "FieldTree.h"
 #include "Atoms.h"
 #include "Atom.h"
 #include "BasePhySystem.h"
 #include "fileio.h"
 
 using namespace fields;
+using namespace fieldtree;
 using namespace atoms;
 using namespace physystem;
 int main(int argc, char *argv[])
 {
   ScalarFieldNode<2> sfn("test", 0.0);
+  FieldTree<2> ft;
   NonInteractingAtoms<PointParticle,2> atoms(100);
-  NonInteractingPhySystem<PointParticle,2> NIPS(&sfn,&atoms);
+  NonInteractingPhySystem<PointParticle,2> NIPS(&ft,&atoms);
   std::srand(std::time(0));
   google::InitGoogleLogging(argv[0]);
   ::testing::InitGoogleTest(&argc, argv);
